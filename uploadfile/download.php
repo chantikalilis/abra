75fd4f7cfcfaaad55c6ff1878e5b92303a6f29c8
chantikalilis <48248682+chantikalilis@users.noreply.github.com>
2021-10-07 23:58:48 +0700

    Add files via upload

---
+++ uploadfile/download.php
@@ -0,0 +1,9 @@
+<?php
+$myDir = "c:/wamp/www/NIM/";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while($tmp = readdir($dir)){
+echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+closedir($dir);
+?>
